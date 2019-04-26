680f3d235e99434ce937cd9dd2acf3ad87d449ad
camilapadilha <camilapadilha1997@gmail.com>
2019-04-25 21:45:33 -0300

    Melhorias noo cadastro de cliente

--- backend/src/api/client/client.js
+++ backend/src/api/client/client.js
@@ -4,7 +4,14 @@ const mongoose = restful.mongoose
 const client = new mongoose.Schema({
     name: { type: String, required: true },
     cpf: { type: String, required: true },
-    rg: { type: String, required: true }
+    rg: { type: String, required: true },
+    endereco: { type: String, required: false },
+    bairro: { type: String, required: false },
+    numero: { type: String, required: false },
+    telefone: { type: String, required: false },
+    celular: { type: String, required: false },
+    tipoPessoa: { type: String, required: false, 
+        enum: ['fisica', 'juridica']}
 })
 
 module.exports = restful.model('Client', client)
--- frontend/src/client/clientForm.jsx
+++ frontend/src/client/clientForm.jsx
@@ -13,19 +13,39 @@ class ClientForm extends Component {
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
+                    <div>
+                        <label>Tipo Pessoa</label>
+                        <div>
+                            <label><Field name="tipoPessoa" component="input" 
+                            type="radio" value="fisica"/> Física</label>
+                            <label><Field name="tipoPessoa" component="input" 
+                            type="radio" value="juridica"/> Jurídica</label>
+                        </div>
+                    </div>
+                    
                     <Field name='name' component={LabelAndInput} readOnly={readOnly}
                     label='Nome' cols='12 4' placeholder='Informe o Nome'/>
                     <Field name='rg' component={LabelAndInput} readOnly={readOnly}
                     label='RG' cols='12 4' placeholder='Informe o RG' />
                     <Field name='cpf' component={LabelAndInput}  readOnly={readOnly}
                     label='CPF' cols='12 4' placeholder='Informe o CPF' />
+                    <Field name='endereco' component={LabelAndInput}  readOnly={readOnly}
+                    label='Endereço' cols='12 4' placeholder='Informe seu Endereço' />
+                    <Field name='bairro' component={LabelAndInput}  readOnly={readOnly}
+                    label='Bairro' cols='12 4' placeholder='Informe seu Bairro' />
+                    <Field name='numero' component={LabelAndInput}  readOnly={readOnly}
+                    label='Número' cols='12 4' placeholder='Informe o Número' />
+                    <Field name='celular' component={LabelAndInput}  readOnly={readOnly}
+                    label='Celular' cols='12 4' placeholder='Informe o Celular' />
+                    <Field name='telefone' component={LabelAndInput}  readOnly={readOnly}
+                    label='Telefone' cols='12 4' placeholder='Informe o Telefone' />
                 </div>
                 <div className='box-footer'>
                     <button type='submit' className={`btn btn-${this.props.submitClass}`}>
                         {this.props.submitLabel}
                     </button>
                     <button type='button' className='btn btn-default'
-                    onCLick={this.props.init}>Cancelar</button>
+                    onClick={this.props.init}>Cancelar</button>
                 </div>
             </form>
 
